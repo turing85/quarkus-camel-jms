f266b4a07bbc088b9ffb0db6f5b5e930a9da93b1
Marco Bungart <marco.bungart@googlemail.com>
2022-10-04 14:40:38 +0200

    configured redelivery on the broker, added route policy

--- receiver/src/main/java/de/turing85/JmsReceiveRoute.java
+++ receiver/src/main/java/de/turing85/JmsReceiveRoute.java
@@ -9,23 +9,29 @@ import java.time.Duration;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
+import javax.jms.ConnectionFactory;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.TransactedDefinition;
 import org.apache.camel.throttling.ThrottlingExceptionRoutePolicy;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @ApplicationScoped
 public class JmsReceiveRoute extends RouteBuilder {
+  private final ConnectionFactory connectionFactory;
   private final PlatformTransactionManager globalPlatformTransactionManager;
   private final AgroalDataSource dataSource;
 
   public JmsReceiveRoute(
+      @SuppressWarnings("CdiInjectionPointsInspection") ConnectionFactory connectionFactory,
+
       @Named(TransactionManagerConfig.GLOBAL_PLATFORM_TRANSACTION_MANAGER_NAME)
       PlatformTransactionManager globalPlatformTransactionManager,
 
       @SuppressWarnings("CdiInjectionPointsInspection")
       @DataSource("data")
       AgroalDataSource dataSource) {
+    this.connectionFactory = connectionFactory;
     this.globalPlatformTransactionManager = globalPlatformTransactionManager;
     this.dataSource = dataSource;
   }
@@ -34,26 +40,32 @@ public class JmsReceiveRoute extends RouteBuilder {
   public void configure() {
     final ThrottlingExceptionRoutePolicy policy = new ThrottlingExceptionRoutePolicy(
         1,
-        Duration.ofSeconds(30).toMillis(),
+        Duration.ofSeconds(1).toMillis(),
         Duration.ofSeconds(10).toMillis(),
-        List.of(Throwable.class));
+        List.of(MyException.class));
     // @formatter:off
-    from(
-        jms("queue:numbers")
+    policy.start();
+    from(jms("queue:numbers")
+            .connectionFactory(connectionFactory)
             .clientId("camel-receiver")
             .advanced()
                 .transactionManager(globalPlatformTransactionManager))
-        .routeId("message-receiver")
         .routePolicy(policy)
+        .routeId("message-receiver")
+        .transacted(TransactedDefinition.PROPAGATION_REQUIRED)
+        .log("policy is started: %b".formatted(policy.isStarted()))
         .log(policy.dumpState())
         .log(LoggingLevel.INFO, "Received: ${body}")
-            .doTry()
-                .to(sql("INSERT INTO data(data) VALUES(:#${body});")
-                    .dataSource(dataSource))
-                .log(LoggingLevel.INFO, "Inserted: ${body}")
-            .doCatch(Exception.class)
-                .markRollbackOnly()
-            .end();
+        .to(sql("INSERT INTO data(data) VALUES(:#${body});")
+            .dataSource(dataSource))
+//        .throwException(new MyException())
+        .log(LoggingLevel.INFO, "Inserted: ${body}");
     // @formatter:on
+  }
+
+  static class MyException extends Exception {
+    MyException() {
+      super("foo");
+    }
   }
 }
